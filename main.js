f31c13ad1b1d1c8097fb33ede365aaed5d216bed
dennis <dennis.olimpio0794@gmail.com>
2020-06-08 15:22:35 +0200

    ho utilizzato la classe active

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ $('.header-right > a').click(
   function()
   {
 
-$('.hamburger-menu').fadeIn();
+$('.hamburger-menu').addClass("active");
 
 });
 
 $(".close").click(
   function(){
 
-    $( ".hamburger-menu").fadeOut();
+    $( ".hamburger-menu").removeClass("active");
 
 }
 );
